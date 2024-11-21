9b1b6fbf2fb6826432a9cb8bf078305c89625968
manoIvans <ivanbarrocaneto10@gmail.com>
2024-11-20 22:12:01 -0300

    Pesquisa
    Adicionei uma barra de pesquisa e uma pasta para os scripts

---
+++ script/pesquisa.js
@@ -0,0 +1,15 @@
+
+const searchLink = document.getElementById('search-link');
+const searchBar = document.getElementById('search-bar'); 
+
+
+searchLink.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block'; 
+    } else {
+        searchBar.style.display = 'none'; 
+    }
+});
